67f6d562ae93fe88aefa8231fe65a08d8bffea02
SylwesterIwanowski <50060453+SylwesterIwanowski@users.noreply.github.com>
2021-12-23 15:06:33 +0100

    add current year

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const name = "Sylwek";
+const year = new Date().getFullYear();
+
+ReactDOM.render(
+  <div>
+    <p>Created by {name}</p>
+    <p>Copyright {year}</p>
+  </div>,
+  document.getElementById("root")
+);
+
 //Create a react app from scratch.
 //It should display 2 paragraph HTML elements.
 //The paragraphs should say:
